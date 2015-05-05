8602172699ce2d240ac57e222ea10e01088996a0
ssdsham <ssd.sham@gmail.com>
2015-05-05 15:31:55 -0400

    tested on 4.0

---
+++ app/src/main/java/cse5236/parkingapp/Location.java
@@ -0,0 +1,227 @@
+package cse5236.parkingapp;
+
+/**
+ * Created by Gregg on 11/10/2014.
+ */
+public class Location {
+
+    /* Private Variables */
+    int id;
+    String name;
+    String coord;
+    String description;
+    double price;
+    String type;
+    String day1s;
+    String day1e;
+    String day2s;
+    String day2e;
+    String day3s;
+    String day3e;
+    String day4s;
+    String day4e;
+    String day5s;
+    String day5e;
+    String day6s;
+    String day6e;
+    String day7s;
+    String day7e;
+    String restriction;
+
+    /* Empty Constructor */
+    public Location(){
+
+    }
+
+    /* Parameterized Constructor */
+    public Location (int id, String name, String coord, String description, double price, String type, String day1s, String day1e, String day2s, String day2e, String day3s, String day3e, String day4s, String day4e, String day5s, String day5e, String day6s, String day6e, String day7s, String day7e, String restriction) {
+        this.id = id;
+        this.name = name;
+        this.coord = coord;
+        this.price = price;
+        this.type = type;
+        this.day1s = day1s;
+        this.day1e = day1e;
+        this.day2s = day2s;
+        this.day2e = day2e;
+        this.day3s = day3s;
+        this.day3e = day3e;
+        this.day4s = day4s;
+        this.day4e = day4e;
+        this.day5s = day5s;
+        this.day5e = day5e;
+        this.day6s = day6s;
+        this.day6e = day6e;
+        this.day7s = day7s;
+        this.day7e = day7e;
+        this.restriction = restriction;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getCoord(){
+        return this.coord;
+    }
+
+    public void setCoord(String coord){
+        this.coord = coord;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public double getPrice(){
+        return this.price;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public String getType(){
+        return this.type;
+    }
+
+    public void setType(String type){
+        this.type = type;
+    }
+
+    public String getDay1s(){
+        return this.day1s;
+    }
+
+    public void setDay1s(String day1s){
+        this.day1s = day1s;
+    }
+
+    public String getDay1e(){
+        return this.day1e;
+    }
+
+    public void setDay1e(String day1e){
+        this.day1e = day1e;
+    }
+
+    public String getDay2s(){
+        return this.day2s;
+    }
+
+    public void setDay2s(String day2s){
+        this.day1s = day2s;
+    }
+
+    public String getDay2e(){
+        return this.day2e;
+    }
+
+    public void setDay2e(String day2e){
+        this.day1e = day2e;
+    }
+
+    public String getDay3s(){
+        return this.day1s;
+    }
+
+    public void setDay3s(String day3s){
+        this.day1s = day3s;
+    }
+
+    public String getDay3e(){
+        return this.day3e;
+    }
+
+    public void setDay3e(String day3e){
+        this.day1e = day3e;
+    }
+
+    public String getDay4s(){
+        return this.day1s;
+    }
+
+    public void setDay4s(String day4s){
+        this.day1s = day4s;
+    }
+
+    public String getDay4e(){
+        return this.day4e;
+    }
+
+    public void setDay4e(String day4e){
+        this.day1e = day4e;
+    }
+
+    public String getDay5s(){
+        return this.day5s;
+    }
+
+    public void setDay5s(String day5s){
+        this.day1s = day5s;
+    }
+
+    public String getDay5e(){
+        return this.day5e;
+    }
+
+    public void setDay5e(String day5e){
+        this.day1e = day5e;
+    }
+
+    public String getDay6s(){
+        return this.day6s;
+    }
+
+    public void setDay6s(String day6s){
+        this.day1s = day6s;
+    }
+
+    public String getDay6e(){
+        return this.day6e;
+    }
+
+    public void setDay6e(String day6e){
+        this.day1e = day6e;
+    }
+
+    public String getDay7s(){
+        return this.day7s;
+    }
+
+    public void setDay7s(String day7s){
+        this.day1s = day7s;
+    }
+
+    public String getDay7e(){
+        return this.day7e;
+    }
+
+    public void setDay7e(String day7e){
+        this.day1e = day7e;
+    }
+
+    public String getRestriction(){
+        return this.restriction;
+    }
+
+    public void setRestriction(String restriction){
+        this.restriction = restriction;
+    }
+}
---
+++ app/src/main/java/cse5236/parkingapp/MapviewActivity.java
@@ -0,0 +1,131 @@
+package cse5236.parkingapp;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+/*Adds a marker to the maps*/
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+import com.google.android.gms.maps.*;
+
+public class MapviewActivity extends Activity {
+
+    /** Local variables **/
+    GoogleMap googleMap;
+    private static final String TAG = "ParkingApp";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        Log.d(TAG, "Got Intent");
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        // Set the text view as the activity layout
+        //setContentView(textView);
+       setContentView(R.layout.mapview);
+        createMapView();
+        addMarker();
+       /* if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        } */
+    }
+
+
+  /*  @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.mapview, menu);
+        return true;
+    } */
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_mapview, container, false);
+            return rootView;
+        }
+    }
+
+    /**
+     * Initialises the mapview
+     */
+    private void createMapView(){
+        /**
+         * Catch the null pointer exception that
+         * may be thrown when initialising the map
+         */
+        try {
+            if(null == googleMap){
+                googleMap = ((MapFragment) getFragmentManager().findFragmentById(
+                        R.id.mapView)).getMap();
+
+                /**
+                 * If the map is still null after attempted initialisation,
+                 * show an error to the user
+                 */
+                if(null == googleMap) {
+                    Toast.makeText(getApplicationContext(),
+                            "Error creating map", Toast.LENGTH_SHORT).show();
+                }
+            }
+        } catch (NullPointerException exception){
+            Log.e("mapApp", exception.toString());
+        }
+    }
+
+    /**
+     * Adds a marker to the map
+     */
+    private void addMarker(){
+
+        /** Make sure that the map has been initialised **/
+        if(null != googleMap){
+            googleMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(0, 0))
+                            .title("Marker")
+                            .draggable(true)
+            );
+        }
+    }
+}
